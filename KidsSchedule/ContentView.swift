575e39ee2dc9018f6832b4144885d6fc11f4e792
sarakluge <Paddel2012>
2021-01-28 13:49:31 +0100

    Initial Commit

---
+++ KidsSchedule/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KidsSchedule
+//
+//  Created by Sara Kluge on 2021-01-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KidsSchedule/KidsScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  KidsScheduleApp.swift
+//  KidsSchedule
+//
+//  Created by Sara Kluge on 2021-01-28.
+//
+
+import SwiftUI
+
+@main
+struct KidsScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
